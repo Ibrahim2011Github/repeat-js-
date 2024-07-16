ef5878bb0675bf2da86991d2fd96d3f2075f695d
Ibrahim2011Github <ibragim.ajdynly@gmail.com>
2024-07-16 13:05:36 +0400

    repeat-js

---
+++ js/main.js
@@ -0,0 +1,41 @@
+document.getElementsByTagName("p")[0].innerHTML = "one"
+document.getElementsByTagName("p")[1].innerHTML = "two"
+document.getElementsByTagName("p")[2].innerHTML = "car"
+document.getElementsByTagName("p")[3].innerHTML = "sell"
+document.getElementsByTagName("p")[4].innerHTML = "tor"
+document.getElementsByTagName("p")[5].innerHTML = "betmen"
+document.getElementsByTagName("span")[0].innerHTML = "span"
+
+
+
+var a = 5
+                //can do
+var a = 25
+console.log(a)
+
+let d = 15
+                // can do
+d = 25
+console.log(d)
+
+// const carname = ["bmw" , "toyota"]
+
+// carname[0] = ["mercedes"]
+// console.log(carname)
+
+// let b = 15
+//                 //can't do with const to
+// let b = 25
+// console.log(b)
+
+if(a===d && a==d){
+    window.alert("hello")
+}else{
+    window.alert('password is wrong')
+}
+
+
+
+
+
+
